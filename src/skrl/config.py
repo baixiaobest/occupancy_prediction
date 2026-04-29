c3931714bf2730709d41e65bcb165d6f738a809b
Baixiao Huang <baixiaohuang1@gmail.com>
2026-04-29 16:23:30 +0800

    update config

--- src/skrl/config.py
+++ src/skrl/config.py
@@ -74,7 +74,7 @@ class SkrlPPOTrainConfig(SkrlTrainConfigBase):
 
 @dataclass
 class SkrlSACTrainConfig(SkrlTrainConfigBase):
-    memory_size: int = 5e5
+    memory_size: int = 4e4
     gradient_steps: int = 1
     batch_size: int = 512
     polyak: float = 0.005
